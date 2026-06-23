a7164cec3ca5351d4b83c792f280f33b175b28fc
quanzaiyu <731734107@qq.com>
2026-06-23 12:57:56 +0800

    feat(mp): add app entry, manifest, pages, scss tokens

---
+++ packages/mp/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { onLaunch, onShow } from '@dcloudio/uni-app';
+
+onLaunch(() => {
+  console.log('Agnes MP launched');
+});
+
+onShow(() => {
+  // 应用进入前台
+});
+</script>
+
+<style lang="scss">
+@import './uni.scss';
+</style>
